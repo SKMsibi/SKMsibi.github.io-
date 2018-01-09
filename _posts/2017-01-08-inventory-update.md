fa72428dd04ff6142991f9855e7b60b34f0798a6
Sabelo <sabelokabelomsibiskm@gmail.com>
2018-01-09 09:06:54 +0000

    added a blog post called inventory update

---
+++ _posts/2017-01-08-inventory-update.md
@@ -0,0 +1,6 @@
+---
+Layout:    
+Title:    "inventory-update"
+Date:    2018-01-08 14:13:08 -0600
+Categories:    
+---
--- tweetnpm.js
+++ tweetnpm.js
@@ -1,12 +1,13 @@
 const request = require('request');
 var express = require('express')
 var tweetExp = express()
- 
+
 const url = "https://api.twitter.com/1.1/statuses/update.json";
 
 var oauth = {
     consumer_key: 'xtSkoYS1EdICn488D0k5w5rrN',
     consumer_secret: '27QiiOaQDueOERNiPX5ImDusAYAKWQqwW4wffpZL6Yz2Ramzg1',
+    
     token_key: '940168325195280385-99ZNHBPGnlbPiG7gorqaUbV4DmRy9q0',
     token_secret: 'NKCHDxXyeV5vVLcuEho9MLTeAY7usYZ4qKNGlPpBtZK67'
 };
@@ -27,7 +28,9 @@ request.post(options,
         }
     });
 tweetExp.get('/', function (request, httpResponse) {
-  res.send('"stay rooted to your roots."')
+    res.send('"stay rooted to your roots."')
 })
- 
-tweetExp.listen(3000);
+
+app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
+
+app.listen(3000, () => console.log('The server started correctly and is listening on port 3000!'))
